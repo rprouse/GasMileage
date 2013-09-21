4863a383f74940437826c4e271802edb8d79ccb9
Rob Prouse <rob@prouse.org>
2013-09-21 14:30:42 -0400

    Vehicle spinner is now displaying one icon for all vehicles. Need to add a selector for setting the icon for a vehicle, need to display that icon and need to save it to the database somehow. See issue #29.

--- GasMileage/src/main/java/net/alteridem/mileage/adapters/VehicleSpinnerAdapter.java
+++ GasMileage/src/main/java/net/alteridem/mileage/adapters/VehicleSpinnerAdapter.java
@@ -1,25 +1,54 @@
 package net.alteridem.mileage.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
+import net.alteridem.mileage.R;
 import net.alteridem.mileage.data.Vehicle;
 
 import java.util.List;
 
-/**
- * Created by Robert Prouse on 13/06/13.
- */
 public class VehicleSpinnerAdapter extends ArrayAdapter<Vehicle> implements SpinnerAdapter {
+
+    private LayoutInflater _inflater;
+
     public VehicleSpinnerAdapter(Context context, List<Vehicle> vehicles) {
         super(context, android.R.layout.simple_spinner_item, vehicles);
-        this.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        _inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public View getDropDownView(int position, View convertView, ViewGroup parent) {
-        return super.getDropDownView(position, convertView, parent);
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if ( view == null ) {
+            view = _inflater.inflate(R.layout.vehicle_spinner_item, parent, false);
+        }
+        Vehicle vehicle = getItem(position);
+        if ( view != null && vehicle != null) {
+            ImageView icon = (ImageView) view.findViewById(R.id.spinner_item_vehicle_icon);
+            TextView name = (TextView) view.findViewById(R.id.spinner_item_vehicle_name);
+
+            name.setText( vehicle.getName() );
+        }
+        return view;
+    }
+
+    public View getDropDownView(int position, View view, ViewGroup parent) {
+        if ( view == null ) {
+            view = _inflater.inflate(R.layout.vehicle_spinner_dropdown_item, parent, false);
+        }
+        Vehicle vehicle = getItem(position);
+        if ( view != null && vehicle != null) {
+            ImageView icon = (ImageView) view.findViewById(R.id.spinner_dropdown_vehicle_icon);
+            TextView name = (TextView) view.findViewById(R.id.spinner_dropdown_vehicle_name);
+
+            name.setText( vehicle.getName() );
+        }
+        return view;
     }
 }
